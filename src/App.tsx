f6b5fff9ca769760c1283a9cb48e1710b66ae0d5
Roman Abdurahmanov <abdurahmanov.roman@gmail.com>
2023-03-06 19:20:22 +0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Navbar'
+import Home from './Home'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='components'>
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
