6cd09d7e70cb6adfb5cd28aa8c318736d4c94005
Roman Abdurahmanov <abdurahmanov.roman@gmail.com>
2023-03-14 10:30:24 +0400

    Added error handling

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from 'react'
 import BlogList, { BlogListType } from './BlogList'
 
 function Home() {
-  const [blogs, setBlogs] = useState<BlogListType>([])
+  const [blogs, setBlogs] = useState<BlogListType | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((res) => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("couldn't fetch data from this resource")
+          }
+          return res.json()
+        })
+        .then((data) => {
+          setBlogs(data)
+          setIsLoading(false)
+          setError(null)
+        })
+        .catch((err) => {
+          setIsLoading(false)
+          setError(err.message)
+        })
+    }, 1000)
   }, [])
 
   return (
     <div className='home'>
+      {error && <div> {error} </div>}
+      {isLoading && <div> Loading... </div>}
       {blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
   )
