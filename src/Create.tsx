d9e8bc58765a2ea66adae98bee262c01e5e0149e
Roman Abdurahmanov <abdurahmanov.roman@gmail.com>
2023-03-20 21:22:02 +0400

    Added forms for blog in Create component

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react'
+
 function Create() {
+  const [title, setTitile] = useState<string>('')
+  const [body, setBody] = useState<string>('')
+  const [author, setAuthor] = useState<string>('mario')
+
   return (
     <div className='create'>
       <h2>Create new blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => {
+            setTitile(e.target.value)
+          }}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value)
+          }}
+        />
+        <label>Author:</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value)
+          }}
+        >
+          <option value='mario'>mario</option>
+          <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
